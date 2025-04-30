d64706d640214f0c0a510b28eac1e77cab06d2e0
Pelangi UV <ilhamirwansyah12@gmail.com>
2025-04-30 12:52:32 +0700

    update pengecekan menu

---
+++ application/controllers/Pengecekan.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+include_once(APPPATH . 'controllers/Auth.php');
+class Pengecekan extends Auth
+{
+    
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function index(){
+    $cab = $this->session->userdata('id_toko');
+    $data['barangcabang'] = $this->second->barangCabang($cab);
+    $data['setcabang'] = $this->first->getCabang();
+    $data['content'] = $this->load->view('transaksi/pengecekan', '', true);
+    $data['modal'] = '';
+    $data['css'] = '<link rel="stylesheet" type="text/css" href="'.base_url('assets/css/vendors/datatables.css').'">
+    <link rel="stylesheet" type="text/css" href="'.base_url('assets/css/vendors/sweetalert2.css').'">';
+    $data['js'] = '<script>var base_url = "' . base_url() . '";</script>
+    <script src="' . base_url('assets/js/additional-js/pengecekan.js?v='.time().) . '"></script>
+    <script src="' . base_url('assets/js/modalpage/validation-modal.js') . '"></script>
+    <script src="' . base_url('assets/js/sweet-alert/sweetalert.min.js').'"></script>
+    <script src="' . base_url('assets/js/datatable/datatables/jquery.dataTables.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.buttons.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/jszip.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/buttons.colVis.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/vfs_fonts.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.autoFill.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.select.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/buttons.bootstrap4.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/buttons.html5.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.bootstrap4.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.responsive.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/responsive.bootstrap4.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.keyTable.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.colReorder.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.fixedHeader.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/dataTables.scroller.min.js') . '"></script>
+    <script src="' . base_url('assets/js/datatable/datatable-extension/custom.js') . '"></script>
+    ';
+    $this->load->view('layout/base', $data);
+  }
+
+}
+
+
+/* End of file pengecekan.php */
+/* Location: ./application/controllers/pengecekan.php */
